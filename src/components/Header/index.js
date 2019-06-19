bfd6e627c023bcfc4a0cb30659b9458751dab69f
Isaac Sunoo <isaacsunoo@gmail.com>
2019-06-18 21:43:44 -0600

    added header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Isaac's Personal Site</h1>
-    </header>
-  )
+    <StickyHeader
+      header={
+        <header className='header'>
+          <h1>Isaac Sunoo</h1>
+          <h4>Software Engineer</h4>
+          <section className='page-links'>
+            <h4>About Me</h4>
+            <h4>Portfolio</h4>
+            <h4>Resume</h4>
+            <h4>Skills</h4>
+            <h4>Contact</h4>
+          </section>
+        </header>
+      }
+    />
+  );
 }
 
 export default Header;
