812504b8e9272c8dbfc838a02c138fa10a0801e8
Isaac Sunoo <isaacsunoo@gmail.com>
2019-06-19 13:02:48 -0600

    added header styling and main page styling

---
+++ src/components/MainPage/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className='main-page'>
+        <div className='entry-image'></div>
+        <h1>Isaac</h1>
+      </div>
+    )
+  }
+}
+
+export default MainPage;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Header from '../../components/Header';
+import MainPage from '../../components/MainPage';
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Route path='/' component={Header} />
+        <Switch>
+          <Route exact path='/' component={MainPage} />
+        </Switch>
       </div>
     );
   }
